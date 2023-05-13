e5023f9bec27dbe7850ae01ab922225b210cb008
abdurrahmankaya71 <abdurrahmanky71@gmail.com>
2023-05-13 16:40:44 +0300

    add new file VAT.java

---
+++ src/Variables/VAT.java
@@ -0,0 +1,24 @@
+package Variables;
+
+import java.util.Scanner;
+
+public class VAT {
+    public static void main(String[] args) {
+        double input, vat_price, vat_amount;
+        System.out.print("Please enter the price to calculate VAT: ");
+        Scanner scan = new Scanner(System.in);
+        input = scan.nextDouble();
+        if(input<=1000 && input>=0){
+            vat_amount = 0.18;
+            vat_price = input + input * vat_amount;
+            System.out.println("VAT amount: "+vat_amount);
+            System.out.println("Total price with VAT: "+vat_price);
+        }
+        else{
+            vat_amount = 0.8;
+            vat_price = input + input * vat_amount;
+            System.out.println("VAT amount: "+vat_amount);
+            System.out.println("Total price with VAT: "+vat_price);
+        }
+    }
+}
