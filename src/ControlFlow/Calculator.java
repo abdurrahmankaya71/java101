2cd404edd65bdebb4f4b7bdebddd1534c3cc2f04
abdurrahmankaya71 <abdurrahmanky71@gmail.com>
2023-05-15 22:29:19 +0300

    add Calculator.java file

---
+++ src/ControlFlow/Calculator.java
@@ -0,0 +1,37 @@
+package ControlFlow;
+
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String[] args) {
+        double num1, num2, resault;
+        int operator;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        num1 = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        num2 = scanner.nextInt();
+        System.out.println("Chose an operator: + , - , / , * ");
+        operator = scanner.next().charAt(0);
+        switch(operator) {
+            case '+':
+                resault = num1 + num2;
+                System.out.println(num1+" + "+num2+" = "+resault);
+                break;
+            case '-':
+                resault = num1 - num2;
+                System.out.println(num1+" - "+num2+" = "+resault);
+                break;
+            case '/':
+                resault = num1 / num2;
+                System.out.println(num1+" / "+num2+" = "+resault);
+                break;
+            case '*':
+                resault = num1 * num2;
+                System.out.println(num1+" * "+num2+" = "+resault);
+                break;
+            default:
+                System.out.println("Invalid operator!");
+                break;
+        }
+    }
+}
